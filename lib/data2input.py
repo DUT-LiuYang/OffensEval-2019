349e0fc1f0090e32b90a2877fc2c3163bc18e4c9
DUT-LiuYang <1396924332@qq.com>
2018-09-30 10:53:19 +0800

    add code which reads sentences and labels from files, and convert label to onehot.

--- lib/data2input.py
+++ lib/data2input.py
@@ -7,3 +7,35 @@ class data2input(object):
     def __init__(self):
         self.dir = "../instance/"
         self.data_dir = "../resource/"
+
+    @staticmethod
+    def read_sentences(file=""):
+        sentences = []
+        rf = open(file, 'w', encoding='utf-8')
+        while True:
+            line = rf.readline()
+            if line == "":
+                break
+            sentences.append(line)
+        rf.close()
+        return sentences
+
+    @staticmethod
+    def read_label(file=""):
+        labels = []
+        rf = open(file, 'w', encoding='utf-8')
+        while True:
+            line = rf.readline()
+            if line == "":
+                break
+            labels.append(int(line))
+        rf.close()
+        return labels
+
+    @staticmethod
+    def label2onehot(labels=[], task=1):
+        onehot_label = []
+        for index, label in enumerate(labels):
+            onehot_label.append([0] * (task + 1))
+            onehot_label[index][label] = 1
+        return  onehot_label
