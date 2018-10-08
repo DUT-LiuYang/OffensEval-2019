d9900141984745473d489dcc42e1df63f51cb5c2
DUT-LiuYang <1396924332@qq.com>
2018-10-08 11:27:33 +0800

    add code which reads sentences and labels from files, and convert label to onehot.

--- lib/PreProcessor.py
+++ lib/PreProcessor.py
@@ -1,4 +1,40 @@
 class PreProcessor:
+    """
+    After reading raw text from the origin files.
+    We adopt the PreProcessor to process the text.
+    """
 
     def __init__(self):
-        pass
+        self.dir = "../instance/"
+
+    @staticmethod
+    def remove_substr(sentence="", len=0, sub="", replace=""):
+        index = sentence.find(sub)
+        while index != -1:
+            temp = sentence[0:index]
+            index = sentence.find(' ', index + len)
+            if index == -1:
+                sentence = temp + replace
+                break
+            else:
+                sentence = temp + replace + sentence[index:]
+                index = sentence.find('@', index)
+
+        return sentence
+
+    @staticmethod
+    def clean_sentence(sentence=""):
+
+        sentence = sentence.replace('$', ' ')
+
+        sentence = PreProcessor.remove_substr(sentence, 2, "@", "@user")
+        sentence = PreProcessor.remove_substr(sentence, 0, "https", "url")
+
+        while "  " in sentence:
+            sentence = sentence.replace('  ', ' ')
+
+        return sentence
+
+
+if __name__ == '__main__':
+    pass
--- lib/data2input.py
+++ lib/data2input.py
@@ -8,10 +8,9 @@ class data2input(object):
         self.dir = "../instance/"
         self.data_dir = "../resource/"
 
-    @staticmethod
-    def read_sentences(file=""):
+    def read_sentences(self, file=""):
         sentences = []
-        rf = open(file, 'w', encoding='utf-8')
+        rf = open(self.data_dir + file, 'w', encoding='utf-8')
         while True:
             line = rf.readline()
             if line == "":
@@ -39,3 +38,35 @@ class data2input(object):
             onehot_label.append([0] * (task + 1))
             onehot_label[index][label] = 1
         return  onehot_label
+
+    def convert_text_to_input(self, sentences=[], max_len=100):
+        tk = Tokenizer(num_words=10000, filters="", split=" ")
+        tk.fit_on_texts(sentences)
+        inputs = tk.texts_to_sequences(sentences)
+        inputs = pad_sequences(inputs, maxlen=max_len, padding='post')
+        self.write_word_index(tk.word_index)
+        self.save_inputs(inputs, self.dir + "train_inputs")
+        return inputs, tk
+
+    def write_word_index(self, word_index={}):
+        word_index_file = self.dir + "word_index.txt"
+        wf = open(word_index_file, 'w', encoding='utf-8')
+        for word, index in word_index.items():
+            wf.write(word + " " + str(index) + "\n")
+        wf.close()
+
+    def save_inputs(self, inputs=[], file=""):
+        wf = open(file, 'w', encoding='utf-8')
+        for line in inputs:
+            for index in line:
+                wf.write(str(index) + " ")
+            wf.write("\n")
+        wf.close()
+
+
+if __name__ == '__main__':
+    x = data2input()
+    print("========== reading text from file. ============")
+    train_sentence_file = "sentences-sub-task1.txt"
+    sentences = x.read_sentences(train_sentence_file)
+    x.convert_text_to_input(sentences, max_len=100)
